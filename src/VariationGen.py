59218ae56569a6d0181e26d14dc9dbde99ea51d4
Aspen <morrisa2@union.edu>
2024-06-29 13:27:55 -0400

    Restructuring and pseudocode.

---
+++ src/VariationGen.py
@@ -0,0 +1,23 @@
+"""
+Module which takes in an Intent and produces semi-random Variations based on
+the parameters of that intent.
+
+Invariant:
+ -  The desired Intent is passed in upon construction, and can be set afterward.
+ -  Any number of variations can be produced for each intent through the generate() function.
+"""
+import Intents.Intent
+import Variation
+import ValConstants as v
+
+def __init__(self,intent):
+    if (intent is type(Intents.Intent)):
+        self.intent = intent
+    else:
+        self.intent = Intents.Intent
+
+def generate(self):
+    pass
+
+def getIntent(self):
+    return self.intent
