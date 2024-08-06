7873d6ab36aad8a70fc408ea589ee380a3172303
Aspen <morrisa2@union.edu>
2024-08-06 12:42:51 -0400

    Modifications to vc.NOTES (very broken).

--- src/test/test_Note.py
+++ src/test/test_Note.py
@@ -3,13 +3,11 @@ import src.main.Note as Note
 
 class NoteTestCase(unittest.TestCase):
 
-    def test_Enharmonic(self):
+    def test_EnharmonicBasics(self):
         """Various test of determining enharmonic equivalence."""
         C4 = Note.Note("C",octave=4)
         Bs3 = Note.Note("B#",octave=3)
         Db4 = Note.Note("Db",octave=4)
-        A4 = Note.Note("A",octave=4)
-        C5 = Note.Note("C",octave=5)
 
         self.assertTrue(C4.overlap(C4),
                         "A note is enharmonically equivalent to itself.")
@@ -17,7 +15,14 @@ class NoteTestCase(unittest.TestCase):
                         "C4 is enharmonically equivalent to B#3.")
         self.assertTrue(C4.overlap(Db4),
                         "C4 is enharmonically equivalent to Db4.")
+
+    def test_EnharmonicsFalse(self):
+        """These notes should not be enharmonically equivalent."""
+        C4 = Note.Note("C", octave=4)
+        A4 = Note.Note("A", octave=4)
+        C5 = Note.Note("C", octave=5)
+
         self.assertFalse(C4.overlap(A4),
-                        "Same octave and accidental, different name.")
+                         "Same octave and accidental, different name.")
         self.assertFalse(C4.overlap(C5),
-                        "Same name and accidental, different octave.")
+                         "Same name and accidental, different octave.")
